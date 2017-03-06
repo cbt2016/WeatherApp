8e4bcb5867bb689e0ab8061c158d3831f08f20c7
Simo hans <simo@gmail.com>
2017-03-06 21:25:14 +0100

    styling examples page

--- app/components/Examples.jsx
+++ app/components/Examples.jsx
@@ -1,9 +1,22 @@
 var React = require('react');
+var {Link} = require('react-router');
 
 var Examples = React.createClass({
   render: function(){
     return (
-      <h4>Examples component </h4>
+      <div>
+        <h1 className="text-center">Examples  </h1>
+        <p>here is a list of locations to try out </p>
+        <ol>
+          <li>
+            <Link to="/?location=los angeles">Los Angeles </Link>
+          </li>
+          <li>
+            <Link to="/?location=rio BRAZIL">rio Brazil </Link>
+          </li>
+        </ol>
+      </div>
+
     );
   }
 });
--- app/components/Main.jsx
+++ app/components/Main.jsx
@@ -7,8 +7,13 @@ var Main = React.createClass({
     return (
       <div>
         <Nav/>
-        <h1>Main component </h1>
-        {this.props.children}
+        <div className="row">
+          <div className="columns medium-6 large-4 small-centered">
+              {this.props.children}
+          </div>
+
+        </div>
+
       </div>
 
     );
