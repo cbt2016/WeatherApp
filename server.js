3853c77c013d20691c22c522b2a35d19086be6fe
Simo hans <simo@gmail.com>
2017-03-06 18:49:49 +0100

    prep for heroku

--- server.js
+++ server.js
@@ -3,11 +3,21 @@
  */
 
 var express = require('express');
+const PORT = process.env.PORT || 3000;
 
 var app = express();
 
+//middleware
+app.use(function(req, res, next){
+  if(req.headers['x-forwarded-proto'] ==='http'){
+    next();
+  }else {
+    res.redirect('http://'+ req.hostname + req.url);
+  }
+})
+
 app.use(express.static('public'));
 
-app.listen(3000,function(){
-    console.log('server is running on port 3000');
+app.listen(PORT,function(){
+    console.log('server is running on port ' + PORT);
 });
